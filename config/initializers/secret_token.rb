3dc21d2d73c83c499576c1b765ece922753d91aa
Chaitanya Vadrevu <chaitu236@tamu.edu>
2014-11-13 00:15:44 +0000

    after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gradesearch::Application.config.secret_token = 'e5202fbad8f326f125a24a54dd7e1827526714940d349df4b52047629e5f4c8215a3974096f6456e22f34fa723846fa6eb13f71065488aa30cd5858c7ee91194'
