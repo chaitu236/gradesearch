a84bada2b7c45e3e399bb626c03794bdad8bdbe5
Chaitanya Vadrevu <chaitu236@tamu.edu>
2016-12-24 22:27:52 +0000

    Update Gemfile

--- precompile.sh
+++ precompile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #RAILS_ENV=production rake assets:precompile
 RAILS_ENV=production bundle exec rake assets:precompile
-rvmsudo rails server -d -p 80 -e production
+#rvmsudo rails server -d -p 2000 -e production
+rails server -d -p 2000 -e production
